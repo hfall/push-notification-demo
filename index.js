d33be5ad6d1683744b2f74750c5be0ebfe8aaa48
hfall <hassanef55@gmail.com>
2022-06-03 15:14:37 -0400

    test

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "client")));
 
 // ROUTES
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/index.html"));
 });
 
