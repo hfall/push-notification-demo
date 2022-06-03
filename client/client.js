bf6800a27956a4434e57c46a7e56afeb7e8a27ff
hfall <hassanef55@gmail.com>
2022-06-03 11:41:13 -0400

    first commit

---
+++ client/client.js
@@ -0,0 +1,50 @@
+const vapidPublicKey =
+  "BFLzeWSWiOuLa_LX1xo7yPm-dqGHk5F1xSxUC2yWaC5IMyP94oTvoM2ci0cFRzEUhvdy5nDyMGnaP_gkhTNeGso";
+
+//check if the serveice worker can work in the current browser
+if ("serviceWorker" in navigator) {
+  init();
+}
+
+//register the service worker, register our push api, send the notification
+async function init() {
+  //register service worker
+  const register = await navigator.serviceWorker.register("/sw.js", {
+    scope: "/",
+  });
+  console.log(register);
+  //register push
+  const subscription = await register.pushManager.subscribe({
+    userVisibleOnly: true,
+    //public vapid key
+    applicationServerKey: urlBase64ToUint8Array(vapidPublicKey),
+  });
+
+  const sendBtn = await document.getElementById("send-js");
+
+  sendBtn.addEventListener("click", async () => {
+    //Send push notification
+    await fetch("/subscribe", {
+      method: "POST",
+      body: JSON.stringify(subscription),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  });
+}
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, "+")
+    .replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
---
+++ index.js
@@ -0,0 +1,51 @@
+//Express
+const express = require("express");
+
+//web-push
+const webpush = require("web-push");
+
+//body-parser
+const bodyParser = require("body-parser");
+
+//path
+const path = require("path");
+
+const env = require("node-env-file");
+
+//using express
+const app = express();
+
+env(__dirname + "/.env");
+
+webpush.setVapidDetails(
+  "mailto:hfall@outlook.com",
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+);
+
+//using bodyparser
+app.use(bodyParser.json());
+//set the static path
+app.use(express.static(path.join(__dirname, "client")));
+
+// ROUTES
+app.post("/subscribe", (req, res) => {
+  //get push subscription object from the request
+  const subscription = req.body;
+
+  //send status 201 for the request
+  res.status(201).json({});
+
+  //create paylod: specified the detals of the push notification
+  const payload = JSON.stringify({ title: "Testing This Push Notification" });
+
+  //pass the object into sendNotification fucntion and catch any error
+  webpush
+    .sendNotification(subscription, payload)
+    .catch((err) => console.error(err));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server started on ${port}`);
+});
