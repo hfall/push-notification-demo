c9e2e06592847f00de7887428f438c7e43adc724
hfall <hassanef55@gmail.com>
2022-06-03 19:15:15 -0400

    update

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post("/subscribe", (req, res) => {
     .catch((err) => console.error(err));
 });
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server started on ${port}`);
 });
